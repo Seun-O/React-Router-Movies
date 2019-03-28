341bd6950279510605b32d51f00fc06e1e9e6010
KingSeun <Seun@Omotundes-MacBook-Pro.local>
2019-03-28 07:36:31 -0400

    Stretch Goals complete

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,12 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route path="/" exact component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route
+          path="/movies/:id"
+          component={props => (
+            <Movie addToSavedList={this.addToSavedList} {...props} />
+          )}
+        />
       </div>
     );
   }
