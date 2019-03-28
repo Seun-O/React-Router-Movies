b003d97bcfa2b8c20e44f73a4f33db5f1e5cfaf3
KingSeun <Seun@Omotundes-MacBook-Pro.local>
2019-03-28 04:24:52 -0400

    MVP complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
 
 export default class Movie extends Component {
   constructor(props) {
@@ -9,9 +10,9 @@ export default class Movie extends Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount(props) {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
@@ -34,33 +35,17 @@ export default class Movie extends Component {
 
   // saveMovie = () => {
   //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  //   addToSavedList(this.state.movie);
+  // };
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard movie={this.state.movie} />
         <div className="save-button">Save</div>
       </div>
     );
